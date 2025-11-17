69ca94d4fcbdc9ecc476528e97095b7f2565c8ce
vvvvvvn <p77111037@gs.ncku.edu.tw>
2025-11-17 11:53:58 +0800

    feat: :art: test to download dataset

--- app/data_collection/dowload_from_kaggle.py
+++ app/data_collection/dowload_from_kaggle.py
@@ -1,6 +1,11 @@
 import kagglehub
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+print(os.environ.get("KAGGLEHUB_CACHE"))  # 測試是否讀到
 # Download latest version
-path = kagglehub.dataset_download("zulkarnainsaurav/four-genre-movie-poster-images")
+path = kagglehub.dataset_download("tmdb/tmdb-movie-metadata")
 
 print("Path to dataset files:", path)
--- app/data_collection/main.py
+++ app/data_collection/main.py
@@ -4,20 +4,25 @@ from imdb import Cinemagoer
 ia = Cinemagoer()
 
 # get a movie
-movie = ia.get_movie("0133093")
-print(movie)
-print(movie.keys())
-# print the names of the directors of the movie
-print("Directors:")
-for director in movie["directors"]:
-    print(director["name"])
+movie = ia.get_movie("111161")  # 肖申克的救贖
+ia.update(movie, info=["main", "full credits", "plot", "vote details"])
 
-# print the genres of the movie
-print("Genres:")
-for genre in movie["genres"]:
-    print(genre)
+print(movie.keys())  # 你會看到 cast 出現
+print(movie.get("cast")[:5])  # 顯示前 5 位演員
 
-# search for a person name
-people = ia.search_person("Mel Gibson")
-for person in people:
-    print(person.personID, person["name"])
+for key, value in movie.items():
+    print(f"{key}: {value}")
+# # print the names of the directors of the movie
+# print("Directors:")
+# for director in movie["directors"]:
+#     print(director["name"])
+
+# # print the genres of the movie
+# print("Genres:")
+# for genre in movie["genres"]:
+#     print(genre)
+
+# # search for a person name
+# people = ia.search_person("Mel Gibson")
+# for person in people:
+#     print(person.personID, person["name"])
